5b4366c20d876416657fb6a52d0e6734457fbd8e
Russias <russiasesteban@gmail.com>
2024-03-26 08:48:30 +0100

    First push

---
+++ BazarJava/src/main/java/com/projetJava/BazarJava/BazarJavaApplication.java
@@ -0,0 +1,13 @@
+package com.projetJava.BazarJava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BazarJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BazarJavaApplication.class, args);
+	}
+
+}
---
+++ BazarJava/src/test/java/com/projetJava/BazarJava/BazarJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projetJava.BazarJava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BazarJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
